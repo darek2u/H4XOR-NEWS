3500fe98845672c6ba794df059257bb727636bfc
Dariusz Urbanczyk <dare2u@gmail.com>
2019-11-21 22:06:15 +0000

    Initial Commit

---
+++ H4XOR NEWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR NEWS
+//
+//  Created by Dariusz Urbanczyk on 21/11/2019.
+//  Copyright © 2019 Dariusz Urbanczyk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
